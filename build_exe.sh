e5d9bcc6fa8b913e069336f29eca1bb671bc5d65
Dustin Spicuzza <dustin@virtualroadside.com>
2017-10-22 22:19:13 -0400

    Fix docker script to change permissions of created exe

--- build_exe.sh
+++ build_exe.sh
@@ -5,5 +5,6 @@
 
 cd $(dirname $0)
 docker run --rm -v "$(pwd):/src:Z" -e PYTHONPATH=/src cdrx/pyinstaller-windows:python2
+docker run --rm -v "$(pwd):/src:Z" --entrypoint /bin/bash cdrx/pyinstaller-windows:python2 -c "chown -R $(id -u $USERNAME) /src/dist"
 
 zip dist/pynetworktables2js-$(git describe --tags).zip dist/windows/pynetworktables2js.exe
